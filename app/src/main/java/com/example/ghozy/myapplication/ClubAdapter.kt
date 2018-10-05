1ed94da180cc3441ef290d00b0382486b873ba46
nabouste <YOU@EXAMPLE.COM>
2018-10-05 14:43:48 +0700

    muncul :camel:

--- app/src/main/java/com/example/ghozy/myapplication/ClubAdapter.kt
+++ app/src/main/java/com/example/ghozy/myapplication/ClubAdapter.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import com.example.ghozy.myapplication.R.id.club_logo
+import com.example.ghozy.myapplication.R.id.club_name
+import com.squareup.picasso.Picasso
 import org.jetbrains.anko.*
 
 class ClubAdapter(private val context: Context, private val items: List<ClubItem>) : RecyclerView.Adapter<ClubAdapter.ClubViewHolder>() {
@@ -24,37 +27,49 @@ class ClubAdapter(private val context: Context, private val items: List<ClubItem
          */
     }
 
-//class ClubUI : AnkoComponent<ViewGroup>{
-//    override fun createView(ui: AnkoContext<ViewGroup>): View {
-//        return with(ui){
-//            linearLayout {
-//                lparams(width = matchParent, height = wrapContent)
-//                padding = dip(16)
-//                orientation = LinearLayout.VERTICAL
-//
-//                imageView {
-//                    id = R.id.club_logo
-//                }.lparams{
-//                    height = dip(50)
-//                    width = dip(50)
-//                }
-//
-//
-//            }
-//        }
-//    }
-//}
+class ClubUI : AnkoComponent<ViewGroup>{
+    override fun createView(ui: AnkoContext<ViewGroup>): View {
+        return with(ui){
+            linearLayout {
+                lparams(width = matchParent, height = wrapContent)
+                padding = dip(16)
+                orientation = LinearLayout.VERTICAL
+
+                imageView {
+                    id = R.id.club_logo
+                }.lparams{
+                    height = dip(50)
+                    width = dip(50)
+                }
+
+                textView{
+                    id = R.id.club_name
+                    textSize = 14f
+                }.lparams{
+                    margin = dip(16)
+                }
+
+
+            }
+        }
+    }
+}
 
 
     override fun getItemCount(): Int = items.size
 
     class ClubViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-//        private val clubLogo: ImageView = view.find(club_logo)
-//        private val clubName: TextView = view.find(club_name)
-//        private val clubDesc: TextView = view.find(club_Desc)
+
+        private val clubLogo: ImageView = view.find(club_logo)
+        private val clubName: TextView = view.find(club_name)
+
 
         fun bindItem(items: ClubItem){
 //            clubLogo.tex
+            clubName.text = items.name
+            items.image?.let {
+                Picasso.get().load(it).into(clubLogo)
+            }
         }
 
 
--- app/src/main/java/com/example/ghozy/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/ghozy/myapplication/MainActivity.kt
@@ -2,11 +2,48 @@ package com.example.ghozy.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import org.jetbrains.anko.ctx
+import org.jetbrains.anko.matchParent
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.verticalLayout
+import org.jetbrains.anko.wrapContent
 
 class MainActivity : AppCompatActivity() {
 
+    private var items: MutableList<ClubItem> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+
+        loadData()
+
+        verticalLayout {
+            recyclerView {
+                lparams(width = matchParent, height = matchParent)
+                layoutManager = LinearLayoutManager(ctx)
+                adapter = ClubAdapter(ctx, items)
+            }
+        }
+
+    }
+
+    private fun loadData() {
+
+        val image = resources.obtainTypedArray(R.array.club_image)
+        val name = resources.getStringArray(R.array.club_name)
+        val desc = resources.getStringArray(R.array.club_desc)
+
+        items.clear()
+        for (i in name.indices) {
+            items.add(ClubItem(name[i],
+                    image.getResourceId(i, 0),
+                    desc[i]))
+        }
+
+        //Recycle the typed array
+        image.recycle()
     }
 }
